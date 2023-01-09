f9eea983ae199aef4aeb38b12779aa690cb34726
amalla4 <amalla4@students.towson.edu>
2023-01-09 14:15:32 -0500

    components for buttons and search input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import './App.css';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className="App">
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-600 to-blue-800 h-fit
+    shadow-2xl shadow-black'>
+      <TopButtons/>
+      <Inputs />
     </div>
   );
 }
