be8dde229e94cc1aa1d23ffc0ee65540550eaa71
amalla4 <amalla4@students.towson.edu>
2023-01-12 00:26:36 -0500

    got api key working. need to fix formatted response

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TempDetails from './components/TempDetails';
 import Forecast from './components/Forecast';
+import getWeatherData from './services/weatherService';
+import getFormattedWeatherData from './services/weatherService';
 
 function App() {
+
+  const fetchWeather = async() => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  }
+
+  //testing api
+  fetchWeather();
+
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-r from-purple-700 to-blue-800 h-fit
     shadow-2xl shadow-black'>
